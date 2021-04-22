02669d6886c3d64d40b95799c79c9653b68ae006
pwzy <2386900869@qq.com>
2021-04-22 20:18:41 +0800

    add zsh some fix

--- zsh/others.zsh
+++ zsh/others.zsh
@@ -1,1 +1,19 @@
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/Users/pwzy/anaconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/Users/pwzy/anaconda3/etc/profile.d/conda.sh" ]; then
+        . "/Users/pwzy/anaconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="/Users/pwzy/anaconda3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
+
+
+eval $(thefuck --alias)
+
 typeset -g POWERLEVEL9K_INSTANT_PROMPT=off
