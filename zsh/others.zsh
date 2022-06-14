955c9b05142ed079183a232c38e1338c966e5e5e
jixuecao <2386900869@qq.com>
2022-06-14 15:04:26 +0800

    Update others.zsh

--- zsh/others.zsh
+++ zsh/others.zsh
@@ -1,28 +1,38 @@
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-__conda_setup="$('/Users/pwzy/anaconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+__conda_setup="$('/home/jing/anaconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
 if [ $? -eq 0 ]; then
     eval "$__conda_setup"
 else
-    if [ -f "/Users/pwzy/anaconda3/etc/profile.d/conda.sh" ]; then
-        . "/Users/pwzy/anaconda3/etc/profile.d/conda.sh"
+    if [ -f "/home/jing/anaconda3/etc/profile.d/conda.sh" ]; then
+        . "/home/jing/anaconda3/etc/profile.d/conda.sh"
     else
-        export PATH="/Users/pwzy/anaconda3/bin:$PATH"
+        export PATH="/home/jing/anaconda3/bin:$PATH"
     fi
 fi
 unset __conda_setup
 # <<< conda initialize <<<
-# conda config --set auto_activate_base false
-#
 
-# # setting for ubuntu
+# conda config --set auto_activate_base false
+
+###################################################################################
 # # setting for brew
 # echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/jing/.zprofile
-# eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-#
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+###################################################################################
+
+###################################################################################
 # # setting for clash
-# alias clash="bash /home/jing/.local/share/clash/clash.sh"
-# export clashdir="/home/jing/.local/share/clash"
+alias clash="bash /home/jing/.local/share/clash/clash.sh"
+export clashdir="/home/jing/.local/share/clash"
+###################################################################################
 
+###################################################################################
+# setting for starship
+eval "$(starship init zsh)"
+###################################################################################
+
+###################################################################################
+# setting for keyboard
 xset r rate 250 50
-
+###################################################################################
